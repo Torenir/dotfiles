e7dca3ef5c8c26b13579494e9bd6dde648cee6ce
Torenir <Torenir@protonmail.com>
2021-05-05 19:49:25 +0200

    2021-05-05 19:49:25

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,9 @@
 set fish_greeting # remove fish greeting at the start
-
-export QT_QPA_PLATFORMTHEME=qt5ct
-set -U fish_user_paths $HOME/.local/bin $fish_user_paths
-set -x MANPAGER "nvim -c 'set ft=man' -"
+export QT_QPA_PLATFORMTHEME=gtk2
+set -gx MANPAGER "nvim -c 'set ft=man' -"
 
 alias lsa='ls -A'
 alias lsblk='lsblk -f'
-alias free='free --mega'
 alias radeontop='radeontop -c'
 alias py='python'
 alias rm='trash-put'
@@ -20,5 +17,5 @@ alias yt-mp3='youtube-dl --add-header 'Cookie:' --extract-audio --audio-format m
 echo; neofetch
 
 if [ (tty) = '/dev/tty1' ]
-	startx
+	startx $HOME/.config/xinitrc
 end
